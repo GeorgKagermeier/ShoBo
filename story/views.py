4d6495e001eeb28912d03166a04f94fe046194d2
Georg Kagermeier <georg-kagermeier@web.de>
2018-06-19 19:32:42 +0200

    Authenticated Login und register

--- story/views.py
+++ story/views.py
@@ -8,43 +8,45 @@ from .models import Story
 
 
 def index(request):
-    #if not request.user.is_authenticated():
-    #    return render(request, 'story/login.html')
-    #else:
-        storys = Story.objects.order_by('-date_added')
-        context = {'storys' : storys}
+    if not request.user.is_authenticated:
+        return render(request, 'registration/login.html')
+    else:
+        stories = Story.objects.order_by('-date_added')
+        context = {'stories': stories}
         return render(request, 'story/index.html', context)
 
+
 def create_story(request):
-    #if not request.user.is_authenticated():
-    #    return render(request, 'story/login.html')
-    #else:
+    if not request.user.is_authenticated:
+        return render(request, 'story/login.html')
+    else:
         form = StoryForm(request.POST or None, request.FILES or None)
         if form.is_valid():
-            story = form.save(commit=False)
-            story.user = request.user
-            story.save()
-            return render(request, 'story/detail.html', {'story': story}),
-
+                story = form.save(commit=False)
+                story.user = request.user
+                story.save()
+                return render(request, 'story/detail.html', {'story': story})
         context = {
-                "form": form,
+            "form": form,
         }
         return render(request, 'story/create_story.html', context)
 
 
 def delete_story(request, story_id):
-    story= Story.objects.get(pk=story_id)
+    story = Story.objects.get(pk=story_id)
     story.delete()
     story = Story.objects.filter(user=request.user)
     return render(request, 'story/index.html', {'story': story})
 
+
 def detail(request, story_id):
-    #if not request.user.is_authenticated():
-    #    return render(request, 'story/login.html')
-    #else:
+    if not request.user.is_authenticated:
+        return render(request, 'registration/login.html')
+    else:
         user = request.user
-        story = get_object_or_404(Story, pk=story_id)
-        return render(request, 'story/detail.html', {'story': story, 'user': user})
+        story = get_object_or_404(Story, pk=story_id),
+    return render(request, 'story/detail.html', {'story': story, 'user': user})
+
 
 def logout_user(request):
     logout(request)
@@ -52,7 +54,7 @@ def logout_user(request):
     context = {
         "form": form,
     }
-    return render(request, 'story/login.html', context)
+    return render(request, 'registration/login.html', context)
 
 
 def login_user(request):
@@ -64,12 +66,13 @@ def login_user(request):
             if user.is_active:
                 login(request, user)
                 stories = Story.objects.filter(user=request.user)
-                return render(request, 'story/index.html', {'storys': stories})
+                return render(request, 'story/index.html', {'stories': stories})
             else:
                 return render(request, 'story/login.html', {'error_message': 'Your account has been disabled'})
         else:
             return render(request, 'story/login.html', {'error_message': 'Invalid login'})
     return render(request, 'story/login.html')
+
 
 def register(request):
     form = UserForm(request.POST or None)
@@ -83,9 +86,9 @@ def register(request):
         if user is not None:
             if user.is_active:
                 login(request, user)
-                storys = Story.objects.filter(user=request.user)
-                return render(request, 'story/index.html', {'storys': storys})
+                stories = Story.objects.filter(user=request.user)
+                return render(request, 'story/index.html', {'stories': stories})
     context = {
         "form": form,
     }
-    return render(request, 'story/registration_form.html', context)
+    return render(request, 'story/register.html', context)

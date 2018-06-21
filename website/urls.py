11a88ec9ae4ebd32284a73571f961e11a6d579cd
Georg Kagermeier <georg-kagermeier@web.de>
2018-06-21 16:17:36 +0200

    Logout implementiert

--- website/urls.py
+++ website/urls.py
@@ -5,8 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('story/', include('story.urls')),
-    path('', include('story.urls')),
+    path('ShoBo/', include('story.urls'))
 
 ]
 

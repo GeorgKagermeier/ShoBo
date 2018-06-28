cb67efc6667697a289f9445d0e476760acba3b80
Georg Kagermeier <georg-kagermeier@web.de>
2018-06-28 18:01:35 +0200

    Remove blindfunction in main.js

--- story/static/story/js/main.js
+++ story/static/story/js/main.js
@@ -6,24 +6,8 @@ var StoryListPage = {
 	},
 
 	render: function() {
-
 	},
 
-	bindEvents: function() {
-		$('.btn-favorite', this.$container).on('click', function(e) {
-			e.preventDefault();
-
-			var self = $(this);
-			var url = $(this).attr('href');
-			$.getJSON(url, function(result) {
-				if (result.success) {
-					$('.glyphicon-star', self).toggleClass('active');
-				}
-			});
-
-			return false;
-		});
-	}
 };
 
 

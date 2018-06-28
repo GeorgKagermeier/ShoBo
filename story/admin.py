a861622c823d59b6d465680c355d54c30813761b
Georg Kagermeier <georg-kagermeier@web.de>
2018-06-28 17:56:12 +0200

    Add docstrings to admin and apps module

--- story/admin.py
+++ story/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Story, Note, Comment
 
+"""
+Admin module register modules which are accessible to the admin
+"""
+
 admin.site.register(Story)
 admin.site.register(Note)
 admin.site.register(Comment)
--- story/apps.py
+++ story/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
+"""
+Apps module contains config for this Django App
+"""
 
 class StoryConfig(AppConfig):
     name = 'story'
